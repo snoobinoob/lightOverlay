829e6863d28188f0203006e953623b7f017d0142
Snoobinoob <cheeseycheese99@gmail.com>
2023-02-22 15:10:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A mod for Necesse.
+
+Check out the [modding wiki page](https://necessewiki.com/Modding) for more.
---
+++ src/main/java/lightOverlay/LightOverlay.java
@@ -0,0 +1,11 @@
+package lightOverlay;
+
+import necesse.engine.modLoader.annotations.ModEntry;
+
+@ModEntry
+public class LightOverlay {
+
+    public void init() {
+        System.out.println("Hello world from Light Overlay!");
+    }
+}
