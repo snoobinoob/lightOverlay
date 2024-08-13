05305ff59f769cad8b05e66d495e269e88580cad
DeadMor0z <13347404+DeadMor0z@users.noreply.github.com>
2024-08-13 17:37:12 +0300

    *Update for game version 0.26.0

--- src/main/java/lightOverlay/patch/LevelDrawUtilsPatch.java
+++ src/main/java/lightOverlay/patch/LevelDrawUtilsPatch.java
@@ -4,14 +4,16 @@ import lightOverlay.LightOverlay;
 import lightOverlay.OverlayDrawable;
 import necesse.engine.modLoader.annotations.ModMethodPatch;
 import necesse.engine.modifiers.Modifier;
-import necesse.engine.tickManager.TickManager;
+import necesse.engine.gameLoop.tickManager.TickManager;
 import necesse.entity.mobs.MobSpawnLocation;
 import necesse.entity.mobs.PlayerMob;
 import necesse.entity.mobs.buffs.BuffModifiers;
 import necesse.gfx.camera.GameCamera;
 import necesse.gfx.drawOptions.texture.SharedTextureDrawOptions;
 import necesse.gfx.drawables.LevelDrawUtils;
-import necesse.gfx.drawables.LevelTileDrawOptions;
+import necesse.gfx.drawables.LevelTileTerrainDrawOptions;
+import necesse.gfx.drawables.LevelTileLightDrawOptions;
+import necesse.gfx.drawables.LevelTileLiquidDrawOptions;
 import necesse.gfx.drawables.OrderableDrawables;
 import necesse.level.maps.Level;
 import net.bytebuddy.asm.Advice;
@@ -19,10 +21,10 @@ import net.bytebuddy.asm.Advice;
 import java.util.List;
 
 public class LevelDrawUtilsPatch {
-    @ModMethodPatch(target = LevelDrawUtils.class, name = "addTileBasedDrawProcesses", arguments = {TickManager.class, GameCamera.class, LevelDrawUtils.DrawArea.class, LevelDrawUtils.DrawArea.class, LevelTileDrawOptions.class, SharedTextureDrawOptions.class, SharedTextureDrawOptions.class, OrderableDrawables.class, List.class, boolean.class, PlayerMob.class})
+    @ModMethodPatch(target = LevelDrawUtils.class, name = "addTileBasedDrawProcesses", arguments = {TickManager.class, GameCamera.class, LevelDrawUtils.DrawArea.class, LevelDrawUtils.DrawArea.class, LevelTileTerrainDrawOptions.class, LevelTileLiquidDrawOptions.class, LevelTileTerrainDrawOptions.class, LevelTileLightDrawOptions.class, SharedTextureDrawOptions.class, SharedTextureDrawOptions.class, OrderableDrawables.class, List.class, boolean.class, PlayerMob.class})
     public static class AddTileBasedDrawProcessesPatch {
         @Advice.OnMethodExit
-        static void onExit(@Advice.FieldValue("level") Level level, @Advice.Argument(1) GameCamera camera, @Advice.Argument(2) LevelDrawUtils.DrawArea tileArea, @Advice.Argument(7) OrderableDrawables list) {
+        static void onExit(@Advice.FieldValue("level") Level level, @Advice.Argument(1) GameCamera camera, @Advice.Argument(2) LevelDrawUtils.DrawArea tileArea, @Advice.Argument(10) OrderableDrawables list) {
             if (!LightOverlay.settings.drawOverlay || level.lightManager.ambientLightOverride != null)
                 return;
             LightOverlay.fakeHostileMob.setLevel(level);
